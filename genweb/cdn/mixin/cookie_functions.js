bae1f76ebf7b44f712139ca0ef45d7d641a2e2f3
avanger9 <ferran.esteban@hotmail.com>
2021-03-17 15:09:49 +0100

    afegit codi per el banner de les cookies i 2 botons: un per acceptar totes les cookies i l'altre per gestionar-les

--- genweb/cdn/mixin/cookie_functions.js
+++ genweb/cdn/mixin/cookie_functions.js
@@ -4,13 +4,12 @@
 
 /*global escape:false, unescape:false */
 
-function createCookie(name,value,days) {
-    var date,
-        expires;
+function createCookie(name, value, days) {
+    var date, expires;
 
     if (days) {
         date = new Date();        
-        date.setTime(date.getTime()+(days*24*60*60*1000));
+        date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires="+date.toGMTString();
     } else {
         expires = "";
@@ -35,3 +34,49 @@ function readCookie(name) {
     }
     return null;
 }
+
+function getCookie(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) c_start = c_value.indexOf(c_name + "=");
+    if (c_start == -1) c_value = null;
+    else {
+      c_start = c_value.indexOf("=", c_start) + 1;
+      var c_end = c_value.indexOf(";", c_start);
+      if (c_end == -1)c_end = c_value.length;
+      c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+  }
+
+function setCookie(c_name, value, exdays){
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+
+
+$(document).ready(function(){
+    // document.cookie = 'ferran'
+    // alert(document.cookie);
+    // document.cookie = 'ferran; expires=Thu, 01 Jan 1970 00:00:01 GMT';
+    // alert(document.cookie);
+
+    // document.cookie = "cookieAccepted=0";
+
+    if (getCookie('cookieAccepted') != "1") {
+        $("#cookies-id").css("display", "block");
+      }
+    else {
+        $("#cookies-id").css("display", "none");
+    }
+
+    $("#cookies-button1").click(function() {
+        // alert("button alert");
+        setCookie('cookieAccepted', '1', 365);
+        $("#cookies-id").css("display", "none");
+        //document.getElementById("cookies-id").remove();
+    });
+});
